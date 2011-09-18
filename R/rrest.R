5563157579640d5099127c47e65abc8b270ebbd3
Moritz Gschwandtner <moritz.gschwandtner@chello.at>
2011-09-18 00:00:00 +0000

    version 1.1

--- R/rrest.R
+++ R/rrest.R
@@ -1,96 +1,113 @@
-########################################################################
-#
-# This file is part of my PhD Thesis
-# description: reimplements the RegMCD algorithm by croux and haesbroeck
-#              with L1 and L2 penalty				
-# author: Moritz Gschwandtner, moritz.gschwandtner@chello.at
-#
-########################################################################
-#require(glasso)
-#require(mvoutlier)
-
-rrest <- function(data, lambda=0.5, alpha=0.75, thresh=0.0001, maxit=10)
+# computes the RegMCD estimator
+rrest <- function(data, lambda=0.5, hp=0.75, thresh=0.0001, maxit=10, penalty="L2")
 {
+
+# this function was taken from croux/haesbroeck and computes the L2 solution
+regL2 <- function(scattermatrix,lambda) 
+{ 
+	prov <- eigen(scattermatrix) 
+	thetai <- (-prov$values+sqrt(prov$values^2+8*lambda))/(4*lambda) 
+	wi <- prov$vectors %*% diag(thetai) %*% t(prov$vectors) 
+	return(list(wi=wi)) 
+	#return(list(wi=wi,w=solve(wi))) 
+}
+
+# computes lp norm of matrix m
+lpnorm <- function(m, p="L2")
+{
+	if(p == "L2")
+	{
+		ret <- sum(m^2)
+	}
+	else
+	{
+		ret <- sum(abs(m))
+	}
+	ret
+}
+
 	## check requirements
 	if(!is.data.frame(data) && !is.matrix(data))
 		stop("parameter data has to be a matrix or data frame!")
 
+	if(penalty == "L2")
+		p_func <- regL2
+	else
+		p_func <- glasso
+
 	data <- as.data.frame(data)
 		
 	n <- dim(data)[1]
-	h <- floor(n*alpha)
+	h <- floor(n*hp)
 	p <- dim(data)[2]
 	n_iter <- 0
 	na_count <- c()
 	
-	if(alpha < 1) ## if h < n we have to do the whole procedure
+	if(hp < 1) ## if h < n we have to do the whole procedure
 	{
-		na_count <- 0
+		na_count <- -1
 		v_loglik <- NA
+		loglik_val <- NA
 		while(is.na(v_loglik))
 		{
 			na_count <- na_count+1
-			# use pcout for outlier detection, and focus on the inner h/2 points
-			pcoutw <- pcout(data)$wfinal
+			pcoutw <- pcout(data)$wfinal # use pcout for outlier detection, and focus on the inner 2 points
 			rsam <- order(pcoutw,decreasing=TRUE)[1:trunc(h/2)]
+
 			rsub <- data[rsam,]
-			## compute inverse covariance matrix using lasso penalty and mean 
 			v_cov <- var(rsub)
-			v_mean <- mean(rsub)
-			v_gl <- glasso(v_cov, rho=lambda)
-			## WARNING: glasso() sometimes produces concentration matrices with negative determinants
-			## this leads to NaN in the following computation!
-			## compute value of objective function
-			v_loglik <- log(det(v_gl$wi)) - mean(mahalanobis(rsub, center=v_mean, cov=v_gl$wi,inverted=TRUE)) - lambda*sum(abs(v_gl$wi))
+			v_mean <- colMeans(rsub)
+			v_gl <- p_func(v_cov, lambda)
+
+			ld <- log(det(v_gl$wi))
+			md <- mean(mahalanobis(rsub, center=v_mean, cov=v_gl$wi,inverted=TRUE))
+			v_loglik <- ld - md - lambda*lpnorm(v_gl$wi, penalty)		
+			loglik_val <- (h/2)*(ld-md)
 		}
 
 		while(n_iter < maxit)
 		{
 			n_iter <- n_iter + 1
-			## compute mahalanobis distances of all observations
 			v_mahal <- mahalanobis(x=data, center=v_mean, cov=v_gl$wi, inverted=TRUE)
 			v_sorted <- sort.int(v_mahal, index.return=TRUE)
-			## now get the next subsample with smallest h distances
 			rsam <- v_sorted$ix[1:h]
 			rsub <- data[rsam,]
-
-			## compute inverse covariance matrix using lasso penalty and mean 
 			v_cov <- var(rsub)
-			v_mean <- mean(rsub)
-			v_gl <- glasso(v_cov, rho=lambda)
-			
-			## compute value of objective function and mean vector
-			mds <- mahalanobis(rsub, center=v_mean, cov=v_gl$wi,inverted=TRUE)
-			v_loglik_new <- log(det(v_gl$wi)) - mean(mds) - lambda*sum(abs(v_gl$wi))
+			v_mean <- colMeans(rsub)
+			v_gl <- p_func(v_cov, lambda)
+			ld <- log(det(v_gl$wi))
+			md <- mean(mahalanobis(rsub, center=v_mean, cov=v_gl$wi,inverted=TRUE))
+			v_loglik_new <- ld - md - lambda*lpnorm(v_gl$wi, penalty)		
+			loglik_val <- (h/2)*(ld-md)
 		
 			## this break statement assures that the algorithm does not terminate with error msg
 			if(is.na( abs((v_loglik_new - v_loglik)/v_loglik) ))
+			{
+				#print("NA produced, while loop left!")
 				break;
-				
+			}	
 			if(as.logical(abs((v_loglik_new - v_loglik)/v_loglik) < thresh))
+			{
+				#print(paste("Break condition reached after", n_iter, "iterations.", sep=" "))
 				break;
-				
+			}	
 			v_loglik <- v_loglik_new
 		}
 	}
-	else ## alpha = 1, so glasso is just computed once
+	else ## hp = 1, so glasso is just computed once
 	{	
 		rsam <- 1:nrow(data)
 		rsub <- data[rsam,]
 		v_cov <- var(rsub)
-		v_mean <- mean(rsub)
-		v_gl <- glasso(v_cov, rho=lambda)
-		v_loglik_new <- log(det(v_gl$wi)) - mean(mahalanobis(rsub, center=v_mean, cov=v_gl$wi,inverted=TRUE)) - lambda*sum(abs(v_gl$wi))
+		v_mean <- colMeans(rsub)
+		v_gl <- p_func(v_cov, lambda)
+		ld <- log(det(v_gl$wi))
+		md <- mean(mahalanobis(rsub, center=v_mean, cov=v_gl$wi,inverted=TRUE))
+		v_loglik_new <- ld - md - lambda*sum((v_gl$wi)^2)
+		loglik_val <- (h/2)*(ld-md)
 		n_iter <- 1
 		nacount <- 0
 	}
-	## compute consistent version
-	cov_cons <- v_gl$w*median(mahalanobis(data, center=v_mean, cov=v_gl$w))/qchisq(0.5, p)
-	covi_cons <- solve(cov_cons) 
-
-	l <- list(mean=v_mean, cov=cov_cons, covi=covi_cons, cov_nocons=v_gl$w, covi_nocons=v_gl$wi, subset=rsam, loglik=v_loglik_new, niter=n_iter, nacount=na_count)
+	l <- list(mean=v_mean, covi_nocons=v_gl$wi, subset=rsam, objective=v_loglik_new, loglik=loglik_val, niter=n_iter)
 	l
 }
-
-
-
--- R/rrlda.R
+++ R/rrlda.R
@@ -1,84 +1,62 @@
-########################################################################
-#
-# This file is part of my PhD Thesis
-# description: It contains the functions rrlda and predict.rrlda 
-#              performing robust regularized linear discriminant 
-#              analysis and the prediction of new observations based on
-#              a model of class rrlda
-# author: Moritz Gschwandtner, moritz.gschwandtner@chello.at
-#
-########################################################################
-
-#source("rrest.R")
-#require(pcaPP)
-#source("haesbroeck.R")
-
-rrlda <- function(x, grouping, prior=NULL, lambda=0.5, alpha=0.75, maxit=50)
+## performs LDA with cov matrix computed by repeated rrest; computes bic value
+rrlda <- function(x, grouping, prior=NULL, lambda=0.5, hp=0.75, nssamples=30, maxit=50, penalty="L2")
 {
-	## check requirements
-    if (is.null(dim(x))) 
-	{
-        stop("'x' is not a matrix or data.frame")
-	}
-	x <- as.matrix(x)
-    if (any(!is.finite(x))) 
-	{
-        stop("Infinite, NA or NaN values in 'x'")
-	}
-	## get dimensions
-	n <- nrow(x)
-    p <- ncol(x)
-	h <- floor(n*alpha)
-	## check if length(x) == length(grouping)
-    if (n != length(grouping)) 
-	{
-        stop("nrow(x) and length(grouping) are different")	
-	}
-	## is grouping a factor?
-	if(!is.factor(grouping))
-	{
-		grouping <- as.factor(grouping)
-	}
-	## get levels, counts ands proportions
-	lev = levels(grouping)
-	k = nlevels(grouping)
-	counts = table(grouping)
-	proportions <- counts/n
+
+# computes repeated rrest and returns object with largest objective value
+rep_rrest <- function(rep=10, data, lambda=0.5, hp=0.75, thresh=0.0001, maxit=10, penalty="L2")
+{
+	max_obj <- -Inf
+	obj <- NULL
 	
-	## check prior probabilities
-	if (!missing(prior)) 
+	
+	for(i in 1:rep)
 	{
-        if (any(prior < 0) || round(sum(prior), 5) != 1) 
+		#print(paste("Lambda=", lambda, "; ", "Repetition: ", i, "\n", sep=""))
+		mrr <- rrest(data=data, lambda=lambda, hp=hp, thresh=thresh, maxit=maxit, penalty=penalty)
+		if(mrr$objective > max_obj)
 		{
-            stop("invalid prior")
+			max_obj <- mrr$objective
+			obj <- mrr
 		}
-        if (length(prior) != k) 
-		{
-            stop("'prior' is of incorrect length")
-		}
-    }
+	}
+	obj
+}
+
+# computes degrees of freedom for concentration matrix m, lambda and penalty p
+degf <- function(m, lambda, p)
+{
+	if(p == "L2")
+	{
+		ret <- alt.df.L2glasso(solve(m), lambda)
+	}
 	else
 	{
-		prior <- proportions
+		ret <- sum(m[upper.tri(m,diag=T)]!=0)
 	}
-	## compute common covariance matrix
-	covm <- matrix(rep(0, p*p), ncol=p)
-	## non consistent version used for bic computation
-	covm_nocons <- matrix(rep(0, p*p), ncol=p)
-	## subset of all observations used  
-	subs <- c()
-	## corresponding subset of grouping variable
-	gr_subs <- c()
+	ret
+}
 
-	## compute location estimates based on RegMCD
+getMeans <- function(gr_means, est_mean, lev, grouping)
+{
+	m_final <- t(t(gr_means) + est_mean)
+	
+	ind_vector <- c()
+	for(i in 1:length(lev))
+	{
+		ind_vector <- c(ind_vector, which(grouping == lev[i]))
+	}
+	m_final <- m_final[ind_vector, ]
+	means <- unique(m_final)
+	rownames(means) <- lev
+	means
+}
+
+# centers the observations using L1 median
+centerObservations <- function(x, k, grouping, lev)
+{
 	m <- t(x)
 	for(i in 1:k)
 	{
-		# we could either calculate the RegMCD estimator for each group (takes very long!) or simply center data by the median
-		#est <- rrest(x[grouping==lev[i],], lambda=lambda, alpha=alpha, penalty.fct=penalty, nssamples=nssamples, maxit=maxit)
-		#m[,grouping==lev[i]] <- est$mean
-		#m[,grouping==lev[i]] <- apply(x[grouping==lev[i],], 2, median)
-		## L1 median: (WARNING: needs at least 2 observations in each group! thats the reason for the if)
 		if(!is.vector(x[grouping==lev[i],]))
 		{
 			m[,grouping==lev[i]] <- l1median_NLM(x[grouping==lev[i],])$par
@@ -90,50 +68,91 @@ rrlda <- function(x, grouping, prior=NULL, lambda=0.5, alpha=0.75, maxit=50)
 	}
 	m <- t(m)
 	z <- x - m
-	
-	## compute covariance matrix of centered observations
-	est <- rrest(z, lambda=lambda, alpha=alpha, maxit=maxit)
-	subs <- x[est$subset,]
-	gr_subs <- grouping[est$subset]
-	covm <- est$cov
-	covm_nocons <- est$cov_nocons
-	
-	# compute final group means
-	m_final <- t(t(m) + est$mean)
-	
-	# create index vector with indices of first, second, etc. level; purpose: sort means afterwards
-	ind_vector <- c()
-	for(i in 1:length(lev))
+	l <- list(z=z, m=m)
+	l
+}
+
+# returns prior probs
+getPriors <- function(pr, k, props)
+{
+	m_prior <- pr
+	if(!is.null(m_prior)) 
 	{
-		ind_vector <- c(ind_vector, which(grouping == lev[i]))
-	}
-	m_final <- m_final[ind_vector, ]
-	
-	# now remove double entries, so we have just one row per group
-	# rows are ordered according to levels; in general this isn't the same order as the groups occur in x
-	means <- unique(m_final)
-	## compute concentration matrix
-	rownames(means) <- lev
-	covi <- solve(covm)
-	covi_nocons <- solve(covm_nocons)
-	
-	## compute bic without penalty term in likelihood function
-	bic <- h*log(det(covi_nocons))
-	for(i in 1:k)
+        if(any(m_prior < 0) || round(sum(m_prior), 5) != 1) 
+		{
+            stop("invalid prior")
+		}
+        if(length(m_prior) != k) 
+		{
+            stop("'prior' is of incorrect length")
+		}
+    }
+	else
 	{
-		bic <- bic - sum(mahalanobis(subs[gr_subs==lev[i],], center=means[i,], cov=covi_nocons, inverted=TRUE))
+		m_prior <- props
 	}
-	nonnuls <- sum(covi_nocons[upper.tri(covi_nocons,diag=TRUE)]!=0)
-	loglik <- (-1)*bic
-	bic <- loglik + log(h)*(k*p + nonnuls)
+	m_prior
+}
+
+# computes degrees of freedom for L2 penalty; computation intense!
+df.L2glasso<-function(sigma,lambda)
+{
+	p=ncol(sigma)
+	G=duplication.matrix(p)
+	a1=kronecker(sigma,sigma)
+	a2=a1+2*lambda*diag(rep(1,p*p))
+	a1=t(G)%*%a1%*%G;
+	a2=t(G)%*%a2%*%G;
+	a=solve(a2)%*%a1
+	df=p+sum(diag(a))
+	return(df)
+}
+
+# alternative: computes degrees of freedom for L2 penalty
+alt.df.L2glasso<-function(sigma,lambda)
+{
+	p=ncol(sigma)
+	eigvalue=eigen(sigma)$val
+	help.matrix=eigvalue%*%t(eigvalue)
+	df=p+sum(lower.triangle(help.matrix/(help.matrix+2*lambda)))
+	return(df)
+}
+	## check requirements
+    if (is.null(dim(x))) 
+        stop("'x' is not a matrix or data.frame")
+
+	x <- as.matrix(x)
+    if (any(!is.finite(x))) 
+        stop("Infinite, NA or NaN values in 'x'")
+
+	n <- nrow(x)
+	p <- ncol(x)
+	h <- floor(n*hp)
 	
-	cl <- match.call()
+    if (n != length(grouping)) 
+        stop("nrow(x) and length(grouping) are different")	
+
+	if(!is.factor(grouping))
+		grouping <- as.factor(grouping)
+
+	lev = levels(grouping)
+	k = nlevels(grouping)
+	counts = table(grouping)
+	proportions <- counts/n
+	
+	co <- centerObservations(x, k, grouping, lev)
+	est <- rep_rrest(rep=nssamples, co$z, lambda=lambda, hp=hp, maxit=maxit, penalty=penalty)
+	means <- getMeans(co$m, est$mean, lev, grouping)
+	bic <- (-2)*est$loglik + log(h)*(k*p + degf(est$covi_nocons, lambda, penalty)) 
+	
+	cl <- match.call()	
     cl[[1L]] <- as.name("rrlda")
-	
-	ret = structure(list(call=cl, prior=prior, counts=counts, means=means, cov=covm, covi=covi, lev=lev, n=n, h=h, bic=bic, loglik=loglik, nonnuls=nonnuls, subs=est$subset), class="rrlda")
+	 
+	ret = structure(list(call=cl, prior=getPriors(prior, k, proportions), counts=counts, means=means, covi=est$covi_nocons, lev=lev, n=n, h=h, bic=bic, loglik=est$loglik, df=degf, subs=est$subset), class="rrlda")
 	ret
 }
 
+# predicts group membership for new data x and object from rrlda
 predict.rrlda <- function(object, x, ...)
 {
 	## check requirements
@@ -166,22 +185,7 @@ predict.rrlda <- function(object, x, ...)
 		x <- as.matrix(x)
 		n <- nrow(x)
 	}
-	
-	#prior_term <- -2*log(t(as.matrix(object$prior)))
-	#mean_term <- diag(object$means %*% object$covi %*% t(object$means))
-	
-	## create matrices for calculation
-	#for(i in 1:n)
-	#{
-	#	if(i != 1)
-	#	{
-	#		prior_term <- rbind(prior_term, -2*log(t(as.matrix(object$prior))))
-	#		mean_term <- rbind(mean_term, diag(object$means %*% object$covi %*% t(object$means)))
-	#	}
-	#}
-	
-	#probs <- -2*t(object$means %*% object$covi %*% t(x)) + mean_term + prior_term
-	
+		
 	probs <- t(-2*object$means %*% object$covi %*% t(x) + diag(object$means %*% object$covi %*% t(object$means)) - 2*log(object$prior[[1]]))
 	cl = object$lev[apply(probs, 1, which.min)]
 	
